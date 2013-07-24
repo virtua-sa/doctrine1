c28b6280ecadcea8e88471097565edf35b92cd72
Ari Pringle <apringle@gmail.com>
2013-07-24 15:03:46 -0600

    replace array_diff to stop notices in PHP 5.4

--- lib/Doctrine/Query/Abstract.php
+++ lib/Doctrine/Query/Abstract.php
@@ -1150,7 +1150,7 @@ abstract class Doctrine_Query_Abstract
         $copy->free();
 
         if ($componentsBefore !== $componentsAfter) {
-            return array_diff($componentsAfter, $componentsBefore);
+            return $this->array_diff_assoc_recursive($componentsAfter, $componentsBefore);
         } else {
             return $componentsAfter;
         }
@@ -2169,4 +2169,29 @@ abstract class Doctrine_Query_Abstract
     {
         $this->disableLimitSubquery = $disableLimitSubquery;
     }
+
+    protected function array_diff_assoc_recursive($array1,$array2)
+    {
+        $difference = array();
+        foreach($array1 as $key => $value) 
+        {
+            if( is_array($value) ) 
+            {
+                if( !isset($array2[$key]) || !is_array($array2[$key]) ) 
+                {
+                    $difference[$key] = $value;
+                } else {
+                    $new_diff = $this->array_diff_assoc_recursive($value, $array2[$key]);
+                    if( !empty($new_diff) ) 
+                    {
+                        $difference[$key] = $new_diff;
+                    }
+                }
+            } else if( !array_key_exists($key,$array2) || $array2[$key] !== $value ) {
+                $difference[$key] = $value;
+            }
+        }
+        return $difference;        
+    }
+
 }
