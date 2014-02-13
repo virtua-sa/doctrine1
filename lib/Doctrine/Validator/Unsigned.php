041485a15f7e6d31e70c005d1a9fc72a27586ab6
Jay Klehr <jay@diablomedia.com>
2014-02-12 17:47:33 -0700

    Using is_numeric instead of preg_match
    The Ticket_1783 test is using PHP_INT_MAX + 1 to test this validator, which is passed as "9.2233720368548E+18" on some systems.  The preg_match that was in place would evaluate as false because of the E and + in the string.

--- lib/Doctrine/Validator/Unsigned.php
+++ lib/Doctrine/Validator/Unsigned.php
@@ -46,7 +46,7 @@ class Doctrine_Validator_Unsigned extends Doctrine_Validator_Driver
         if (is_null($value) || $value == '') {
             return true;
         }
-        if (preg_match('/[^0-9\-\.]/', $value)) {
+        if (!is_numeric($value)) {
             return false;
         }
 
